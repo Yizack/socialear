d3a6c173cc412afae8cd5372a7958934d14a68a3
Yizack Rangel <yizackr@gmail.com>
2019-09-15 23:30:09 -0500

    Update combine.php

--- functions/combine.php
+++ functions/combine.php
@@ -1,21 +1,16 @@
 <?php
-if(isset($_POST["submit"])) {
-  if(move_uploaded_file($_FILES['input_video']['tmp_name'] , basename($_FILES['input_video']['name'])) && move_uploaded_file($_FILES['input_audio']['tmp_name'] , basename($_FILES['input_audio']['name']))) {
-    $cmd = "ffmpeg -y -i ".basename($_FILES['input_video']['name'])." -i ".basename($_FILES['input_audio']['name'])." -c copy -map 0:v -map 1:a output.mp4";
-    echo "<script>console.log('Debug Objects: " . $cmd . "' );</script>";
+    $video = "http://socialearytapi.epizy.com/videoplayback.mp4";
+    $audio = "http://socialearytapi.epizy.com/videoplayback.m4a";
+    $cmd = "ffmpeg -y -i ".$video." -i ".$audio." -c copy -map 0:v -map 1:a output.mp4";
     system($cmd);
-  }
-  else{
-    echo "error uploading";
-  }
 }
 ?>
 <html>
   <body>
     <form action="/functions/combine" method="POST" enctype="multipart/form-data">
-      <input type="file" name="input_video" placeholder="video"><br><br>
-      <input type="file" name="input_audio" placeholder="audio"><br><br>
-      <input type="submit" name="submit">
+      <input type="file" name="input_video" id="input_video" placeholder="video"><br><br>
+      <input type="file" name="input_audio" id="input_audio" placeholder="audio"><br><br>
+      <button type="submit" name="submit">Combinar</button>
     </form>
   </body>
 </html>
