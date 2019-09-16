2511029e5784d31afeb4de622072f8f045389a0f
Yizack Rangel <yizackr@gmail.com>
2019-09-15 22:15:55 -0500

    Update combine.php

--- functions/combine.php
+++ functions/combine.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST["submit"])) {
+if(isset($_POST["submit"]) && isset($_FILES['input_video']) && isset($_FILES['input_audio'])) {
   $cmd = "ffmpeg -y -i ".basename($_FILES['input_video']['name'])." -i ".basename($_FILES['input_audio']['name'])." -c copy -map 0:v -map 1:a output.mp4";
 }
  /*   $filename = $_POST['filename'];  
@@ -12,7 +12,7 @@ if(isset($_POST["submit"])) {
 ?>
 <html>
   <body>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST" enctype="multipart/form-data">
       <input type="file" name="input_video" placeholder="video"><br><br>
       <input type="file" name="input_audio" placeholder="audio"><br><br>
       <input type="submit" name="submit">
