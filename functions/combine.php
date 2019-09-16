541c989f7c82ade2aa94b06b2a194523a68afd2e
Yizack Rangel <yizackr@gmail.com>
2019-09-15 22:29:08 -0500

    Update combine.php

--- functions/combine.php
+++ functions/combine.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST["submit"]) && isset($_FILES['input_video']) && isset($_FILES['input_audio'])) {
-  if(move_uploaded_file($_FILES['input_video']['tmp_name'] , "") && move_uploaded_file($_FILES['input_audio']['tmp_name'] , "")) {
+  if(move_uploaded_file($_FILES['input_video']['tmp_name'] , basename($_FILES['input_video']['name'])) && move_uploaded_file($_FILES['input_audio']['tmp_name'] , basename($_FILES['input_audio']['name']))) {
     $cmd = "ffmpeg -y -i ".basename($_FILES['input_video']['name'])." -i ".basename($_FILES['input_audio']['name'])." -c copy -map 0:v -map 1:a output.mp4";
   }
 }
