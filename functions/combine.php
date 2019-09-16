14691399e19760398a921e494bf6826edae61acd
Yizack Rangel <yizackr@gmail.com>
2019-09-16 00:48:53 -0500

    ftp ffmpeg

--- functions/combine.php
+++ functions/combine.php
@@ -1,8 +1,16 @@
 <?php
-    $video = "http://socialearytapi.epizy.com/videoplayback.mp4";
-    $audio = "http://socialearytapi.epizy.com/videoplayback.mp3";
+    ftp://[user[:password]@]server[:port]/path/to/remote/resource.mpeg
+    $user = getenv('FTP_USER');
+    $password = getenv('FTP_PASSWORD');
+    $host = getenv('FTP_HOST');
+    $port = getenv('FTP_PORT');
+    $path = getenv('FTP_PATH');
+    $ftp = "ftp://$user:$password@$host:$port/$path";
+
+    $video = "$ftp/videoplayback.mp4";
+    $audio = "$ftp/videoplayback.mp3";
     $cmd = "ffmpeg -y -i $video -i $audio -c copy -map 0:v -map 1:a output.mp4";
-    echo $cmd;
+
     system($cmd);
 ?>
 <html>
