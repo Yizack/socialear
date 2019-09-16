85ac44c221d2f985d3f6df8b7abc99846b255150
Yizack Rangel <yizackr@gmail.com>
2019-09-15 20:33:24 -0500

    Update download.php

--- functions/download.php
+++ functions/download.php
@@ -1,7 +1,7 @@
   <?php
     $filename = $_POST['filename'];  
     $url = $_POST['url'];
-    header('Content-Type: application/octet-stream');
+    header('Content-Type: video/mp4');
     header("Content-Transfer-Encoding: Binary"); 
     header("Content-disposition: attachment; filename=\"".$filename."\""); 
     readfile($url);
