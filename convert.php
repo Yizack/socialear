58f290fcbe67a954a5aa84447d96f45ba50a3a89
Yizack Rangel <yizackr@gmail.com>
2019-09-19 01:16:42 -0500

    Update convert.php

--- convert.php
+++ convert.php
@@ -1,5 +1,25 @@
+<?php
+$files = glob("ffmpeg/output/*");
+$now   = time();
+foreach ($files as $file) {
+	if (is_file($file)) {
+      if ($now - filemtime($file) >= 60 * 10) { // 10 min
+    	unlink($file);
+    	}
+    }
+}
+?>
 <html>
   <body>
+  <?php
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+      if (isset($_GET["token"])) {
+        $token = $_GET["token"];
+        echo "    <p>Here is your converted video: <a href='/ffmpeg/output/video-$token.mp4'>video-$token.mp4</a></p>\n";
+        echo "    <p>Note: You have 10 minutes to download, then it will be deleted.</p>\n";
+      }
+    }
+  ?>
     <form action="http://socialearytapi.epizy.com/" method="POST" enctype="multipart/form-data">
       VIDEO<input type="file" name="input_video" placeholder="video"><br><br>
       AUDIO<input type="file" name="input_audio" placeholder="audio"><br><br>
