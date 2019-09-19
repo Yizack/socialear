2164635783c92a2bd84bfd3ebd44f1908524e643
Yizack Rangel <yizackr@gmail.com>
2019-09-19 01:43:48 -0500

    init output

--- ffmpeg/combine.php
+++ ffmpeg/combine.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
 		$video = "$ftp/video-$token.mp4";
 		$audio = "$ftp/video-$token.mp3";
-		unlink("outputvideo-278309ebdc0ab827.mp4");
+		unlink("video-9e8e030673600415.mp4");
 		system("cd output");
 		$cmd = "ffmpeg -y -i $video -i $audio -c copy -map 0:v -map 1:a video-$token.mp4";
 		system($cmd);
