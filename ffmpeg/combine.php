e35e9a529b742625aed035a245ea06125c59b4a9
Yizack Rangel <yizackr@gmail.com>
2019-09-19 01:15:53 -0500

    combine audio and video requesting a token

---
+++ ffmpeg/combine.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	if (isset($_GET["token"])) {
+		$token = $_GET["token"];
+		$user = getenv('FTP_USER');
+		$password = getenv('FTP_PASSWORD');
+		$host = getenv('FTP_HOST');
+		$port = getenv('FTP_PORT');
+		$path = getenv('FTP_PATH');
+		$ftp = "ftp://$user:$password@$host:$port/$path";
+
+		$video = "$ftp/video-$token.mp4";
+		$audio = "$ftp/video-$token.mp3";
+		$cmd = "ffmpeg -y -i $video -i $audio -c copy -map 0:v -map 1:a output/video-$token.mp4";
+		system($cmd);
+	}
+}
+?>
