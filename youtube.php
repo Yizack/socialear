eb7e9b2368b17ea9c8e710a0d75646dcba331861
Yizack Rangel <yizackr@gmail.com>
2019-09-27 18:10:46 -0500

    popper js

--- youtube.php
+++ youtube.php
@@ -184,6 +184,7 @@
     <script src="/js/jquery-3.4.1.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
     <script src="https://kit.fontawesome.com/cc91b92ca8.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script>
       $(document).ready(function(){
         $('[data-toggle="popover"]').popover();   
