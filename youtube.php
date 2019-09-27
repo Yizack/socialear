6cdc03baf0a0412bd413158a04914c2f25b9dc5e
Yizack Rangel <yizackr@gmail.com>
2019-09-27 18:14:24 -0500

    Update youtube.php

--- youtube.php
+++ youtube.php
@@ -182,9 +182,9 @@
       </div>
     </footer>
     <script src="/js/jquery-3.4.1.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
     <script src="https://kit.fontawesome.com/cc91b92ca8.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
     <script>
       $(document).ready(function(){
         $('[data-toggle="popover"]').popover();   
