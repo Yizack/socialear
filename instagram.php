b182d90edcf550b792732a86fb74efefe36a730c
Yizack Rangel <yizackr@gmail.com>
2019-10-12 16:11:41 -0500

    download from private accounts

--- instagram.php
+++ instagram.php
@@ -1,22 +1,20 @@
 <?php
-  require("functions/global.php"); // Global functions
-  // Language
-  if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    if (isset($_GET["lang"])) {
-      switch($_GET["lang"]) {
-        case "es": require("strings/spanish.php"); break;
-        case "it": require("strings/italian.php"); break;
-        case "pt": require("strings/portuguese.php"); break;
-        case "fr": require("strings/french.php"); break;
-        case "de": require("strings/german.php"); break;
-        case "nl": require("strings/dutch.php"); break;
-        default: require("strings/english.php"); break;
-      }
-    }
-    else {
-      require("strings/english.php");
-    }
+require("functions/global.php"); // Global functions
+// Language
+if (isset($_GET["lang"])) {
+  switch($_GET["lang"]) {
+    case "es": require("strings/spanish.php"); break;
+    case "it": require("strings/italian.php"); break;
+    case "pt": require("strings/portuguese.php"); break;
+    case "fr": require("strings/french.php"); break;
+    case "de": require("strings/german.php"); break;
+    case "nl": require("strings/dutch.php"); break;
+    default: require("strings/english.php"); break;
   }
+}
+else {
+  require("strings/english.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="<?= $code; ?>">
@@ -55,6 +53,12 @@
     <section class="video_download text-white">
       <div class="container">
         <div class="row align-items-center">
+          <div class="col-lg-12 order-lg-2">
+            <div class="alert alert-secondary alert-dismissible">
+              <button type="button" class="close" data-dismiss="alert">&times;</button>
+              <i class="fas fa-info-circle fa-fw"></i> If the video is from a private account, <a href="/instagram?private">click here</a> to know how to download it.
+            </div>
+          </div>
           <div class="col-lg-6 order-lg-1">
             <div class="text-center p-5">
               <h2><?= $video_instagram; ?></h2>
@@ -80,11 +84,14 @@
             <h2 class="text-center p-5"><?= $more_instagram; ?></h2>
           </div>
           <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-            <form action="<?php if ($code !== "en"){e("/".$code);} ?>/instagram" method="get">
+<?php
+if (isset($_GET["private"])) {
+?>
+            <form action="<?php if ($code !== "en"){e("/".$code);} ?>/instagram" method="post">
               <div class="form-row">
-                <div class="col-12 col-md-9 mb-2 mb-md-0">
+                <div class="col-12 col-md-12 mb-3">
                   <div class="input-group">
-                    <input class="form-control form-control" type="text" id="URL" placeholder="https://instagram.com/p/B0qVP-4ncRi" name="URL">
+                    <textarea class="form-control" rows="5" id="code" name="code"></textarea>
                   </div>
                 </div>
                 <div class="col-12 col-md-3">
@@ -92,6 +99,25 @@
                 </div>
               </div>
             </form>
+<?php
+}
+else {
+?>
+            <form action="<?php if ($code !== "en"){e("/".$code);} ?>/instagram" method="get">
+              <div class="form-row">
+                <div class="col-12 col-md-9 mb-2 mb-md-0">
+                  <div class="input-group">
+                    <input class="form-control" type="text" id="URL" placeholder="https://instagram.com/p/B0qVP-4ncRi" name="URL">
+                  </div>
+                </div>
+                <div class="col-12 col-md-3">
+                  <button class="btn btn-success btn-block" type="submit"><?= $download; ?></button>
+                </div>
+              </div>
+            </form>
+<?php
+}
+?>
           </div>
         </div>
       </div>
@@ -120,6 +146,23 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         $("#video_options").append('<div class="input-group m-3"><div class="input-group-prepend"><span class="input-group-text text-monospace">MP4</span></div><div class="input-group-append"><a class="btn btn-primary" href="'+ video + '" role="button"><i class="fas fa-download fa-fw"></i><?= $download ?></a></div></div>');
       });
     </script>
+<?php
+  }
+}
+else {
+  if (isset($_POST["code"])) {
+    $data = $_POST["code"];
+?>
+  <script>
+    var data = "<?= $data ?>";
+    var url = $(data).filter('meta[property="og:url"]').attr("content");
+    var thumbnail = $(data).filter('meta[property="og:image"]').attr("content");
+    var title = $(data).filter('meta[property="og:title"]').attr("content");
+    var video = $(data).filter('meta[property="og:video"]').attr("content");
+    $("#video_title").append(title);
+    $("#video_image").append('<a href="' + url + '" target="_blank"><img class="img-fluid d-inline" src="'+ thumbnail +'" alt="' + title + '" title="' + title + '" /></a>');
+    $("#video_options").append('<div class="input-group m-3"><div class="input-group-prepend"><span class="input-group-text text-monospace">MP4</span></div><div class="input-group-append"><a class="btn btn-primary" href="'+ video + '" role="button"><i class="fas fa-download fa-fw"></i><?= $download ?></a></div></div>');
+  </script>
 <?php
   }
 }
