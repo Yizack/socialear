816ea62b00601b7713740af14f1ca4709624cd58
Yizack Rangel <yizackr@gmail.com>
2020-02-05 21:56:52 -0500

    Update force.php

--- force.php
+++ force.php
@@ -4,14 +4,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET'){
 	$token = $_GET["token"];
 	$aws = "https://s3.us-east-2.amazonaws.com";
     $location = "$aws/$bucket/file-$token";
-    $title = $_GET['title'];
+    $title = urldecode($_GET['title']);
     header("Pragma: public");
     header('Expires: 0');
     header('Cache-Control: no-store, no-cache, must-revalidate');
     header('Cache-Control: pre-check=0, post-check=0, max-age=0');
     header("Cache-Control: public");    
     header("Content-Description: File Transfer");
-    header("Content-Type: application/octet-stream");
+    header("Content-Type: audio/mp3");
     header("Content-Length: ".filesize($location));
     header("Content-Disposition: attachment; filename=$title");
     // Force the download           
